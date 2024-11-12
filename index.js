dad487111565f2bbf46476c175f23d219e1e1f98
Claudio128 <cllaudio128@gmail.com>
2024-11-12 02:54:40 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,98 @@
+function pacotemisto0() {
+  document.getElementById("img").src = "./src/1.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto1() {
+  document.getElementById("img").src = "./src/2.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto2() {
+  document.getElementById("img").src = "./src/3.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto3() {
+  document.getElementById("img").src = "./src/4.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto4() {
+  document.getElementById("img").src = "./src/5.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto5() {
+  document.getElementById("img").src = "./src/6.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto6() {
+  document.getElementById("img").src = "./src/7.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto7() {
+  document.getElementById("img").src = "./src/8.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto8() {
+  document.getElementById("img").src = "./src/9.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto9() {
+  document.getElementById("img").src = "./src/10.png";
+  console.log("Funcionando");
+}
+
+function pacotemisto10() {
+  document.getElementById("img").src = "./src/11.png";
+  console.log("Funcionando");
+}
+
+setTimeout(pacotemisto1, 2000);
+
+setTimeout(pacotemisto2, 5000);
+
+setTimeout(pacotemisto3, 8000);
+
+setTimeout(pacotemisto4, 11000);
+
+setTimeout(pacotemisto5, 14000);
+
+setTimeout(pacotemisto6, 17000);
+
+setTimeout(pacotemisto7, 20000);
+
+setTimeout(pacotemisto8, 23000);
+
+setTimeout(pacotemisto9, 26000);
+
+setTimeout(pacotemisto10, 29000);
+
+// Primeira repetição
+
+setTimeout(pacotemisto0, 32000);
+
+setTimeout(pacotemisto1, 35000);
+
+setTimeout(pacotemisto2, 38000);
+
+setTimeout(pacotemisto3, 41000);
+
+setTimeout(pacotemisto4, 44000);
+
+setTimeout(pacotemisto5, 47000);
+
+setTimeout(pacotemisto6, 50000);
+
+setTimeout(pacotemisto7, 53000);
+
+setTimeout(pacotemisto8, 56000);
+
+setTimeout(pacotemisto9, 59000);
+
+setTimeout(pacotemisto10, 62000);
